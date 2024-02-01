6306a51d0ccd934c84f4f46bdcdfc5f2c078876a
Wene MacV <wenetis@gmail.com>
2024-01-31 18:21:38 -0700

    Update product-form.js

--- assets/product-form.js
+++ assets/product-form.js
@@ -97,7 +97,7 @@ if (!customElements.get('product-form')) {
             let freeGiftParentIDs = localStorage.getItem("freeGiftParentIDs");
             let freeGiftId = localStorage.getItem("freeGiftId");
             let giftItemAvailableInCart = localStorage.getItem("giftItemAvailableInCart")
-            if (!giftItemAvailableInCart && freeGiftParentIDs?.length && freeGiftId && isBlackColor && isMediumSize)  {
+            if (!giftItemAvailableInCart && freeGiftParentIDs?.length && freeGiftId && isBlackColor)  {
               console.log({freeGiftParentIDs, freeGiftId})
               const ADD_FREE_GIFT = freeGiftParentIDs.includes(response.id);
               console.log("ADD FREE GIFT", ADD_FREE_GIFT)
