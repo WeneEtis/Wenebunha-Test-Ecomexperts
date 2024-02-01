2bcde86d880d9820a12791ad7ae86cc4470e7319
Wene MacV <wenetis@gmail.com>
2024-01-31 17:09:39 -0700

    Update cart.js

--- assets/cart.js
+++ assets/cart.js
@@ -102,7 +102,7 @@ class CartItems extends HTMLElement {
       },
     ];
   }
-
+/*
   updateQuantity(line, quantity, name, variantId) {
     this.enableLoading(line);
 
@@ -171,6 +171,117 @@ class CartItems extends HTMLElement {
       })
       .catch(() => {
         this.querySelectorAll('.loading__spinner').forEach((overlay) => overlay.classList.add('hidden'));
+        const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
+        errors.textContent = window.cartStrings.error;
+      })
+      .finally(() => {
+        this.disableLoading(line);
+      });
+  }*/
+  async updateQuantity(line, quantity, name, variantId) {
+    this.enableLoading(line);
+    
+    // FREE GIFT SCRIPT STARTS
+    let freeGiftParentIDs = localStorage.getItem("freeGiftParentIDs");
+    let freeGiftId = localStorage.getItem("freeGiftId");
+    if (freeGiftParentIDs) {
+      let lineItem = document.getElementById('main-cart-items').querySelectorAll("tr")[line]
+      let url = new URL(lineItem.querySelector('a').href)
+      let deletedId = new URLSearchParams(url.search)?.get('variant')
+      
+      const REMOVE_GIFT_ITEM = freeGiftParentIDs.includes(deletedId) && quantity == 0;
+      console.log({includes: freeGiftParentIDs.includes(deletedId), REMOVE_GIFT_ITEM})
+      
+      if (REMOVE_GIFT_ITEM) {
+        try {
+          let res = await fetch(window.Shopify.routes.root + 'cart/update.js', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              updates: {
+                [freeGiftId]: 0,
+                [deletedId]: 0
+              }
+            })
+          })
+          localStorage.removeItem("giftItemAvailableInCart")
+          let response = await res.json()
+          console.log({response})
+          window.location.reload()
+        } catch (error) {
+          console.log({error})
+        }
+      }
+    }
+    // FREE GIFT SCRIPT ENDS
+  
+    const body = JSON.stringify({
+      line,
+      quantity,
+      sections: this.getSectionsToRender().map((section) => section.section),
+      sections_url: window.location.pathname,
+    });
+  
+    fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
+      .then((response) => {
+        return response.text();
+      })
+      .then((state) => {
+        const parsedState = JSON.parse(state);
+        const quantityElement =
+          document.getElementById(`Quantity-${line}`) || document.getElementById(`Drawer-quantity-${line}`);
+        const items = document.querySelectorAll('.cart-item');
+  
+        if (parsedState.errors) {
+          quantityElement.value = quantityElement.getAttribute('value');
+          this.updateLiveRegions(line, parsedState.errors);
+          return;
+        }
+  
+        this.classList.toggle('is-empty', parsedState.item_count === 0);
+        const cartDrawerWrapper = document.querySelector('cart-drawer');
+        const cartFooter = document.getElementById('main-cart-footer');
+  
+        if (cartFooter) cartFooter.classList.toggle('is-empty', parsedState.item_count === 0);
+        if (cartDrawerWrapper) cartDrawerWrapper.classList.toggle('is-empty', parsedState.item_count === 0);
+  
+        this.getSectionsToRender().forEach((section) => {
+          const elementToReplace =
+            document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+          elementToReplace.innerHTML = this.getSectionInnerHTML(
+            parsedState.sections[section.section],
+            section.selector
+          );
+        });
+        const updatedValue = parsedState.items[line - 1] ? parsedState.items[line - 1].quantity : undefined;
+        let message = '';
+        if (items.length === parsedState.items.length && updatedValue !== parseInt(quantityElement.value)) {
+          if (typeof updatedValue === 'undefined') {
+            message = window.cartStrings.error;
+          } else {
+            message = window.cartStrings.quantityError.replace('[quantity]', updatedValue);
+          }
+        }
+        this.updateLiveRegions(line, message);
+  
+        const lineItem =
+          document.getElementById(`CartItem-${line}`) || document.getElementById(`CartDrawer-Item-${line}`);
+        if (lineItem && lineItem.querySelector(`[name="${name}"]`)) {
+          cartDrawerWrapper
+            ? trapFocus(cartDrawerWrapper, lineItem.querySelector(`[name="${name}"]`))
+            : lineItem.querySelector(`[name="${name}"]`).focus();
+        } else if (parsedState.item_count === 0 && cartDrawerWrapper) {
+          trapFocus(cartDrawerWrapper.querySelector('.drawer__inner-empty'), cartDrawerWrapper.querySelector('a'));
+        } else if (document.querySelector('.cart-item') && cartDrawerWrapper) {
+          trapFocus(cartDrawerWrapper, document.querySelector('.cart-item__name'));
+        }
+  
+        publish(PUB_SUB_EVENTS.cartUpdate, { source: 'cart-items', cartData: parsedState, variantId: variantId });
+      })
+      .catch(() => {
+        this.querySelectorAll('.loading-overlay').forEach((overlay) => overlay.classList.add('hidden'));
         const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
         errors.textContent = window.cartStrings.error;
       })
